081103ff8f0d35a0c30fe39510a3cf80a548490b
Dr. Omugi <43462266+yasuouchida@users.noreply.github.com>
2026-05-11 12:51:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -374,7 +374,7 @@ inject_css()
 # ============================================================
 
 st.markdown('<a id="top"></a>', unsafe_allow_html=True)
-st.title("Scratch sb3 変数改行編集ツール")
+st.title("Scratch sb3 変数改行編集ツール V.0.1")
 st.caption("Streamlit版：sb3内の変数に、改行やスペースを含む文字列を保存します。")
 
 with st.expander("使い方", expanded=False):
